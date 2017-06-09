061e180576319a2dade15b1f326b0b1c13307f13
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2017-06-09 20:53:26 +0300

    Refactoring & pixel taming

--- src/ktindex/KtIndexWidget.kt
+++ src/ktindex/KtIndexWidget.kt
@@ -18,6 +18,7 @@ import com.intellij.util.ui.GraphicsUtil
 import com.intellij.util.ui.JBUI
 import java.awt.*
 import java.awt.geom.GeneralPath
+import java.awt.geom.Line2D
 import java.io.IOException
 import java.util.concurrent.TimeUnit
 import javax.swing.JComponent
@@ -26,8 +27,10 @@ import javax.swing.SwingConstants
 import javax.swing.SwingUtilities
 import javax.swing.border.Border
 
-class KtIndexWidget(private val myProject: Project) : JLabel("   ", EmptyIcon.ICON_16, SwingConstants.LEADING), ProjectComponent, CustomStatusBarWidget, StatusBarWidget, DumbAware, Border {
-    private var myParser: Runnable? = null
+class KtIndexWidget(private val myProject: Project) : JLabel("   ", EmptyIcon.ICON_16, SwingConstants.LEADING), ProjectComponent, CustomStatusBarWidget, StatusBarWidget, DumbAware, Border, Runnable {
+    private val KT_TIOBE_WHAT = "kt.tiobe.what"
+    private val KT_TIOBE_WHEN = "kt.tiobe.when"
+    private val ARROW = '\u2192'
 
     override fun projectOpened() {
         WindowManager.getInstance().getStatusBar(myProject).addWidget(this, "before " + PositionPanel.ID)
@@ -38,31 +41,32 @@ class KtIndexWidget(private val myProject: Project) : JLabel("   ", EmptyIcon.IC
         repaint()
     }
 
-    override fun initComponent() {
-        myParser = Runnable {
-            try {
-                val text = initText()
+    override fun run() {
+        try {
+            val text = initText()
 
-                SwingUtilities.invokeLater {
-                    icon = IconLoader.getIcon("K.png")
-                    font = if (SystemInfo.isMac) JBUI.Fonts.smallFont() else JBUI.Fonts.miniFont()
-                    iconTextGap = -getFontMetrics(font).stringWidth("#") / 2 - 1
-                    if (!font.canDisplay(ARROW)) {
-                        setText(text.replace("" + ARROW, "->") + "  ")
-                    } else {
-                        setText(text + "  ")
-                    }
-
-                    border = this
-                    revalidate()
-                    repaint()
+            SwingUtilities.invokeLater {
+                icon = IconLoader.getIcon("K.png")
+                font = if (SystemInfo.isMac) JBUI.Fonts.smallFont() else JBUI.Fonts.miniFont()
+                iconTextGap = -getFontMetrics(font).stringWidth("#") / 2 - 1
+                if (!font.canDisplay(ARROW)) {
+                    setText(text.replace("" + ARROW, "->") + "  ")
+                } else {
+                    setText(text + "  ")
                 }
-                JobScheduler.getScheduler().schedule(myParser!!, 23, TimeUnit.HOURS)
-            } catch (e: IOException) {
-                JobScheduler.getScheduler().schedule(myParser!!, 1, TimeUnit.HOURS)
+
+                border = this
+                revalidate()
+                repaint()
             }
+            JobScheduler.getScheduler().schedule(this, 23, TimeUnit.HOURS)
+        } catch (e: IOException) {
+            JobScheduler.getScheduler().schedule(this, 1, TimeUnit.HOURS)
         }
-        JobScheduler.getScheduler().schedule(myParser!!, 1, TimeUnit.SECONDS)
+    }
+
+    override fun initComponent() {
+        JobScheduler.getScheduler().schedule(this, 1, TimeUnit.SECONDS)
     }
 
     @Throws(IOException::class)
@@ -125,16 +129,27 @@ class KtIndexWidget(private val myProject: Project) : JLabel("   ", EmptyIcon.IC
     }
 
     override fun paintBorder(c: Component, g: Graphics, x: Int, y: Int, width: Int, height: Int) {
+        val m = (height - .5f) / 2
         val g2d = g as Graphics2D
-        g2d.paint = GradientPaint(x.toFloat(), y.toFloat(), Color(223, 110, 0), x.toFloat(), y + height - .5f, Color(90, 73, 173))
-        val path = GeneralPath()
-        val m = (height - .5) / 2.0
-        path.moveTo((x + JBUI.scale(15)).toFloat(), y.toFloat())
-        path.lineTo((x + width).toDouble() - 1.0 - m, y.toDouble())
-        path.lineTo((width - 1).toDouble(), y + m)
-        path.lineTo((x + width).toDouble() - 1.0 - m, y + 2 * m)
-        path.lineTo((x + JBUI.scale(15)).toDouble(), y + 2 * m)
         GraphicsUtil.setupAAPainting(g2d)
+        g2d.paint = GradientPaint(
+                (x + JBUI.scale(7)).toFloat(), y.toFloat(), Color(223, 110, 0, 0),
+                (x + JBUI.scale(14)).toFloat(), y.toFloat(), Color(223, 110, 0))
+        g2d.draw(Line2D.Float((x + JBUI.scale(7)).toFloat(), y.toFloat(), (x + JBUI.scale(14)).toFloat(), y.toFloat()))
+        g2d.paint = GradientPaint(
+                (x + JBUI.scale(7)).toFloat(), y + 2f * m, Color(90, 73, 173, 0),
+                (x + JBUI.scale(14)).toFloat(), y + 2f * m, Color(90, 73, 173))
+        g2d.draw(Line2D.Float((x + JBUI.scale(7)).toFloat(), y + 2 * m, (x + JBUI.scale(14)).toFloat(), y + 2 * m))
+
+        g2d.paint = GradientPaint(
+                x.toFloat(), y.toFloat(), Color(223, 110, 0),
+                x.toFloat(), y + height - .5f, Color(90, 73, 173))
+        val path = GeneralPath()
+        path.moveTo((x + JBUI.scale(14)).toFloat(), y.toFloat())
+        path.lineTo((x + width - 1).toFloat() - m, y.toFloat())
+        path.lineTo((x + width - 1).toFloat(), y + m)
+        path.lineTo((x + width - 1).toFloat() - m, y + 2 * m)
+        path.lineTo((x + JBUI.scale(14)).toFloat(), y + 2 * m)
         g2d.draw(path)
     }
 
@@ -146,12 +161,5 @@ class KtIndexWidget(private val myProject: Project) : JLabel("   ", EmptyIcon.IC
     }
 
     override fun projectClosed() {
-    }
-
-    companion object {
-
-        private val KT_TIOBE_WHAT = "kt.tiobe.what"
-        private val KT_TIOBE_WHEN = "kt.tiobe.when"
-        private val ARROW = '\u2192'
     }
 }
